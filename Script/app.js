731b4310f0de62a3e0db0d50a8e98823f9d20353
MadalinaMP <madalinapuimp@gmail.com>
2020-09-22 15:33:58 +0300

    removed comments

--- Script/app.js
+++ Script/app.js
@@ -1,82 +1,64 @@
-//this app doesn't work properly
-//it has no OOP elements because I didnt know how to mix them
-//this was made strictly based on the blog example
-//i tried following each step by adding comments to make sure i understood the logic
-//where i added an (?) i am uncertain of my interpretation and i would like a validation
-
-const container = document.querySelector("#generatedGame"); //the container for the games
-const addGameButton = document.querySelector("#buton"); //the button that adds new games
-
 function getGames() {
-  //we need to return the fetch in order to call the function and add .then (?)
   return fetch("https://games-world.herokuapp.com/games").then(function (
     response
   ) {
-    return response.json(); //un-string the response (from json object to js object)
+    return response.json();
   });
 }
 
+const container = document.querySelector("#generatedGame");
 getGames().then(function (games) {
-  //the object returned : 'games'
-  console.log(games); //an array of games; check if it's displayed in the console
+  console.log(games);
 
   games.forEach(function (game) {
-    //for each game in the array:
-    const gameDOM = createGameDOM(game); //1.we create a new game DOM (that's like a template function) - to be put in the document
-
-    const deleteButton = createDeleteButton(); //2. we create a delete button
+    const gameDOM = createGameDOM(game);
+    const deleteButton = createDeleteButton();
     deleteButton.addEventListener("click", function () {
-      deleteGame(game._id) //3. on click, we delete the game by id (?)
+      deleteGame(game._id)
         .then(function (response) {
-          console.log(response); // 4. we pick the response from the deleteGame function (?)
-          container.removeChild(gameDOM); //5. we remove the game DOM from the container
+          // console.log(response);
+          container.removeChild(gameDOM);
         })
         .catch(function (err) {
-          console.log(err); //error handling
+          // console.log(err);
         });
     });
-    gameDOM.appendChild(deleteButton); //6. we add the buttom to the game DOM
-
-    container.appendChild(gameDOM); //7. we add the game DOM to the main container
+    gameDOM.appendChild(deleteButton);
+    container.appendChild(gameDOM);
   });
 });
 
 function createGameDOM(game) {
-  //game refers to a game element. whether it is already from the server's list or input (?)
-  const gameDOM = document.createElement("div"); //we make a div named game DOM
-  //we populate the div with what we need
+  const gameDOM = document.createElement("div");
   gameDOM.innerHTML = `
     <h2>${game.title}</h2>
     <p>${game.description}<p>`;
 
-  gameDOM.classList.add("gameStyle"); //added style to the DOM
+  gameDOM.classList.add("gameStyle");
 
-  return gameDOM; //we need to return the const in order to create new game DOMs in other functions (?)
+  return gameDOM;
 }
 
 function createDeleteButton() {
-  const deleteButton = document.createElement("button"); //we create a button
-  deleteButton.innerText = "Delete"; //we add text to the button
-  deleteButton.classList.add("buton"); //we style the button with a class
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "Delete";
+  deleteButton.classList.add("buton");
 
-  return deleteButton; //we need to return the const in order to be able to create buttons with this function in other functions(?)
+  return deleteButton;
 }
 
 function deleteGame(gameId) {
-  //we delete a game based on its ID. we need to return the fetch in order to be able to call this function somewhere else(?)
-  //depends on which game we pick, the ID will vary
   return fetch(`https://games-world.herokuapp.com/games/${gameId}`, {
     method: "DELETE",
   }).then(function (response) {
-    return response.text(); //this is the response from the server. we used .text() because we received a string
+    return response.text();
   });
 }
 
 function getGameData() {
-  const gameTitle = document.querySelector("#title").value; //we get the value from the title input
-  const gameDescription = document.querySelector("#description").value; //we get the value from the text area
-  //we return an object of this form that will be populated with the said inputs we gave
-  //we need to return this object in order to be able to use this function in another one (?)
+  const gameTitle = document.querySelector("#title").value;
+  const gameDescription = document.querySelector("#description").value;
+
   return {
     title: gameTitle,
     description: gameDescription,
@@ -84,32 +66,30 @@ function getGameData() {
 }
 
 function saveGameOnServer(game) {
-  //'game' refers to the game we input (?)
-  //we make a promise const that fetches the server url and adds the input game on it (?)
   const promise = fetch("https://games-world.herokuapp.com/games", {
     method: "POST",
     headers: {
       "Content-Type": "application/x-www-form-urlencoded",
+      "Accept": "*/*",
     },
     body: `title=${game.title}&description=${game.description}`,
   }).then(function (response) {
-    return response.json(); //after we add it to the server we get the whole response (the game) and turn it into a JS object that can be added to the document (?)
+    return response.json();
   });
 
-  return promise; //we need to return the promise in order to be able to call this function in another one (?)
+  return promise;
 }
 
-addGameButton.addEventListener("click", createGame); //for the button that adds games, on click we call the createGame function
+const addGameButton = document.querySelector("#buton");
+addGameButton.addEventListener("click", createGame);
 function createGame() {
-  const game = getGameData(); //we create a new const that creates a new game object that takes its values from the inputs
-  console.log(game); //we print it in the console to check
-
-  //we call the save on server function in order to add the input game to it
+  const game = getGameData();
+  // console.log(game);
   saveGameOnServer(game)
     .then(function (response) {
-      console.log(response); //after the function does its part, we get the JS object response and check it in the console
+      // console.log(response);
     })
     .catch(function (err) {
-      console.log(err); //error handling just in case
+      // console.log(err);
     });
 }
